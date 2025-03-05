a25b0ee3e04586f07410eb55c8c8f810b8b7d740
berat-erkul <beraterkul00@gmail.com>
2025-03-05 21:12:24 +0100

    LAB starter

---
+++ src/test/java/com/cydeo/ormlab/OrmLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ormlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
